yaovicoder/status-react

[fix #3913] do not logout when changing mailserver
- do not logout and remove previous mailserver
from peers when changing mailserver
- rename wnode mailserver
- move transport.inbox to mailserver.core
- fix all subs and db keys

src/status_im/data_store/realm/schemas/base/migrations.cljs
@@ -90,3 +90,18 @@
 
 (defn v12 [old-realm new-realm]
   (log/debug "migrating base database v12: " old-realm new-realm))
+
+(defn v13
+  "Rename wnode to mailserver in account settings"
+  [old-realm new-realm]
+  (log/debug "migrating accounts schema v13")
+  (let [accounts (.objects new-realm "account")]
+    (dotimes [i (.-length accounts)]
+      (let [account      (aget accounts i)
+            {:keys [wnode] :as old-settings} (deserialize (aget account "settings"))
+            new-settings (when wnode
+                           (-> old-settings
+                               (dissoc :wnode)
+                               (assoc :mailserver wnode)))
+            updated      (serialize new-settings)]
+        (aset account "settings" updated)))))

test/cljs/status_im/test/transport/core.cljs
@@ -26,24 +26,24 @@
                              :name "name-3"
                              :address "address-3"
                              :password "password-3"}
-            expected-wnodes {:eth.beta {"1" (-> ms-1
-                                                (dissoc :fleet)
-                                                (assoc :user-defined true))
-                                        "2" (-> ms-2
-                                                (dissoc ms-2 :fleet)
-                                                (assoc :user-defined true))}
-                             :eth.test {"3" (-> ms-3
-                                                (dissoc :fleet)
-                                                (assoc :user-defined true))}}
+            expected-mailservers {:eth.beta {"1" (-> ms-1
+                                                     (dissoc :fleet)
+                                                     (assoc :user-defined true))
+                                             "2" (-> ms-2
+                                                     (dissoc ms-2 :fleet)
+                                                     (assoc :user-defined true))}
+                                  :eth.test {"3" (-> ms-3
+                                                     (dissoc :fleet)
+                                                     (assoc :user-defined true))}}
             cofx-with-ms    (assoc cofx
                                    :data-store/mailservers
                                    [ms-1
                                     ms-2
                                     ms-3])]
-        (is (= expected-wnodes
+        (is (= expected-mailservers
                (-> (get-in
                     (protocol/initialize-protocol cofx-with-ms "user-address")
-                    [:db :inbox/wnodes])
+                    [:db :mailserver/mailserver])
                    (update-in [:eth.beta "1"] dissoc :generating-sym-key?)
                    (update-in [:eth.beta "2"] dissoc :generating-sym-key?)
                    (update-in [:eth.test "3"] dissoc :generating-sym-key?))))))))

BMValero/status-mobile

Signed-off-by: yenda <eric@status.im>

src/status_im/data_store/realm/schemas/base/migrations.cljs
@@ -93,3 +93,18 @@
 
 (defn v13 [old-realm new-realm]
   (log/debug "migrating base database v13: " old-realm new-realm))
+
+(defn v14
+  "Rename wnode to mailserver in account settings"
+  [old-realm new-realm]
+  (log/debug "migrating accounts schema v14")
+  (let [accounts (.objects new-realm "account")]
+    (dotimes [i (.-length accounts)]
+      (let [account      (aget accounts i)
+            {:keys [wnode] :as old-settings} (deserialize (aget account "settings"))
+            new-settings (when wnode
+                           (-> old-settings
+                               (dissoc :wnode)
+                               (assoc :mailserver wnode)))
+            updated      (serialize new-settings)]
+        (aset account "settings" updated)))))

test/cljs/status_im/test/transport/core.cljs
@@ -32,24 +32,24 @@
                              :name "name-3"
                              :address "address-3"
                              :password "password-3"}
-            expected-wnodes {:eth.beta {"1" (-> ms-1
-                                                (dissoc :fleet)
-                                                (assoc :user-defined true))
-                                        "2" (-> ms-2
-                                                (dissoc ms-2 :fleet)
-                                                (assoc :user-defined true))}
-                             :eth.test {"3" (-> ms-3
-                                                (dissoc :fleet)
-                                                (assoc :user-defined true))}}
+            expected-mailservers {:eth.beta {"1" (-> ms-1
+                                                     (dissoc :fleet)
+                                                     (assoc :user-defined true))
+                                             "2" (-> ms-2
+                                                     (dissoc ms-2 :fleet)
+                                                     (assoc :user-defined true))}
+                                  :eth.test {"3" (-> ms-3
+                                                     (dissoc :fleet)
+                                                     (assoc :user-defined true))}}
             cofx-with-ms    (assoc cofx
                                    :data-store/mailservers
                                    [ms-1
                                     ms-2
                                     ms-3])]
-        (is (= expected-wnodes
+        (is (= expected-mailservers
                (-> (get-in
                     (protocol/initialize-protocol cofx-with-ms "user-address")
-                    [:db :inbox/wnodes])
+                    [:db :mailserver/mailservers])
                    (update-in [:eth.beta "1"] dissoc :generating-sym-key?)
                    (update-in [:eth.beta "2"] dissoc :generating-sym-key?)
                    (update-in [:eth.test "3"] dissoc :generating-sym-key?))))))))