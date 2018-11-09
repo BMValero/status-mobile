yaovicoder/status-react

[refactor] rename whisper-id and whisper-identity to public-key

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -196,6 +196,18 @@
           browser/v8
           dapp-permissions/v9])
 
+(def v20 [chat/v8
+          transport/v7
+          mailserver-topic/v1
+          contact/v3
+          message/v7
+          mailserver/v11
+          user-status/v2
+          membership-update/v1
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -253,4 +265,7 @@
                :migration     migrations/v18}
               {:schema        v19
                :schemaVersion 19
-               :migration     migrations/v19}])
+               :migration     migrations/v19}
+              {:schema        v20
+               :schemaVersion 20
+               :migration     migrations/v20}])

src/status_im/data_store/realm/schemas/account/user_status.cljs
@@ -10,3 +10,14 @@
                       :chat-id          :string
                       :whisper-identity :string
                       :status           :string}})
+
+(def v1 {:name       :user-status
+         :primaryKey :status-id
+         :properties {;; Unfortunately, realm doesn't support composite primary keys,
+                      ;; so we have to keep separate `:status-id` property, which is just
+                      ;; `:message-id`-`:public-key` concatenated
+                      :status-id        :string
+                      :message-id       :string
+                      :chat-id          :string
+                      :public-key       :string
+                      :status           :string}})

BMValero/status-mobile

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -226,6 +226,19 @@
           browser/v8
           dapp-permissions/v9])
 
+(def v23 [chat/v8
+          transport/v7
+          contact/v3
+          message/v7
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v1
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -292,4 +305,7 @@
                :migration     migrations/v21}
               {:schema        v22
                :schemaVersion 22
-               :migration     migrations/v22}])
+               :migration     migrations/v22}
+              {:schema        v23
+               :schemaVersion 23
+               :migration     migrations/v23}])

src/status_im/data_store/realm/schemas/account/user_status.cljs
@@ -10,3 +10,14 @@
                       :chat-id          :string
                       :whisper-identity :string
                       :status           :string}})
+
+(def v2 {:name       :user-status
+         :primaryKey :status-id
+         :properties {;; Unfortunately, realm doesn't support composite primary keys,
+                      ;; so we have to keep separate `:status-id` property, which is just
+                      ;; `:message-id`-`:public-key` concatenated
+                      :status-id        :string
+                      :message-id       :string
+                      :chat-id          :string
+                      :public-key       :string
+                      :status           :string}})