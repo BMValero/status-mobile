yaovicoder/status-react

[refactor] remove email field in account

src/status_im/data_store/realm/schemas/base/account.cljs
@@ -207,3 +207,5 @@
 (def v13 (assoc-in v12
                    [:properties :desktop-notifications?]
                    {:type :bool :default false}))
+
+(def v14 (update v13 :properties dissoc :email))

BMValero/status-mobile

src/status_im/data_store/realm/schemas/base/account.cljs
@@ -211,3 +211,5 @@
 (def v14 (assoc-in v13
                    [:properties :desktop-alpha-release-warning-shown?]
                    {:type :bool :default false}))
+
+(def v15 (update v14 :properties dissoc :email))