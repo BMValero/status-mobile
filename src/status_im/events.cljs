yaovicoder/status-react

Show debug information when LES is selected (in the titlebar).
To simplify testing and debugging, there is a very simple sync indicator
(debug) for LES. This UI is not even close to being production and will
be reworked in the future.

src/status_im/events.cljs
@@ -28,6 +28,8 @@
             [status-im.protocol.core :as protocol]
             [status-im.qr-scanner.core :as qr-scanner]
             [status-im.signals.core :as signals]
+            [status-im.node.core :as node]
+            [status-im.web3.core :as web3]
             [status-im.ui.screens.currency-settings.models
              :as
              currency-settings.models]
@@ -103,6 +105,14 @@
  :init.callback/keychain-reset
  (fn [cofx _]
    (init/initialize-keychain cofx)))
+
+;; home screen
+
+(handlers/register-handler-fx
+ :home.ui/sync-info-pressed
+ (fn [cofx name]
+   (log/debug "igorm -> sync info pressed" name)
+   (node/display-les-debug-info cofx)))
 
 ;; accounts module
 
@@ -617,7 +627,12 @@
 (handlers/register-handler-fx
  :web3.callback/get-syncing-success
  (fn [cofx [_ error sync]]
-   (protocol/update-sync-state cofx error sync)))
+   (web3/update-syncing-progress cofx error sync)))
+
+(handlers/register-handler-fx
+ :web3.callback/get-block-number
+ (fn [cofx [_ error block-number]]
+   (node/update-block-number cofx error block-number)))
 
 ;; notifications module
 

BMValero/status-mobile

Signed-off-by: Igor Mandrigin <i@mandrigin.ru>

src/status_im/events.cljs
@@ -36,6 +36,8 @@
             [status-im.signals.core :as signals]
             [status-im.transport.message.core :as transport.message]
             [status-im.ui.screens.currency-settings.models :as currency-settings.models]
+            [status-im.node.core :as node]
+            [status-im.web3.core :as web3]
             [status-im.ui.screens.navigation :as navigation]
             [status-im.utils.fx :as fx]
             [status-im.utils.handlers :as handlers]
@@ -111,6 +113,13 @@
  :init.callback/keychain-reset
  (fn [cofx _]
    (init/initialize-keychain cofx)))
+
+;; home screen
+
+(handlers/register-handler-fx
+ :home.ui/sync-info-pressed
+ (fn [cofx _]
+   (node/display-les-debug-info cofx)))
 
 ;; accounts module
 
@@ -706,7 +715,12 @@
 (handlers/register-handler-fx
  :web3.callback/get-syncing-success
  (fn [cofx [_ error sync]]
-   (protocol/update-sync-state cofx error sync)))
+   (web3/update-syncing-progress cofx error sync)))
+
+(handlers/register-handler-fx
+ :web3.callback/get-block-number
+ (fn [cofx [_ error block-number]]
+   (node/update-block-number cofx error block-number)))
 
 ;; notifications module
 