BMValero/status-mobile

commit 8626cd3e6db0ca894a99c7348035c7bf1e9ebb18

Show unread indicator on Activity Center tabs (#14883)
Fixes https://github.com/status-im/status-mobile/issues/14852
Fixes https://github.com/status-im/status-mobile/issues/14882

Summary
=======

- [x] Fixes https://github.com/status-im/status-mobile/issues/14852
- [x] Fixes https://github.com/status-im/status-mobile/issues/14882
- [x] Refactors `tab` component to follow new guidelines.
- [x] Improves preview area for tabs to optionally show notification dots.
- [x] Refactors `tabs` component: break the component into smaller chunks;
remove duplication between scrollable & non-scrollable tabs; update outdated
docstring;
- [x] Adds accessibility labels to tabs.

Cutout with light theme on the background:
https://user-images.githubusercontent.com/46027/214553974-2b2e8c6e-71b8-46f3-ba75-a66cb4755490.png

Cutout with dark theme on the background:
https://user-images.githubusercontent.com/46027/214554602-d637d4d6-3b20-4aa8-a2ef-64836f4454bb.png

Cutout in non-scrollable tabs:
https://user-images.githubusercontent.com/46027/214555646-bbb85546-20d9-48bb-8273-716564fb5e0f.png

Notification dots in preview area:
https://user-images.githubusercontent.com/46027/214556532-10a044ae-9df5-4c86-be5f-447955276bfd.png

src/quo2/components/tabs/segmented_tab.cljs
@@ -1,5 +1,5 @@
 (ns quo2.components.tabs.segmented-tab
-  (:require [quo2.components.tabs.tab :as tab]
+  (:require [quo2.components.tabs.tab.view :as tab]
             [quo2.foundations.colors :as colors]
             [quo2.theme :as theme]
             [react-native.core :as rn]
@@ -28,7 +28,7 @@
            [rn/view
             {:margin-left (if (= 0 indx) 0 2)
              :flex        1}
-            [tab/tab
+            [tab/view
              {:id        id
               :segmented true
               :size      size

src/quo2/components/tabs/tab/style.cljs
@@ -1,0 +1,101 @@
+(ns quo2.components.tabs.tab.style
+  (:require [quo2.foundations.colors :as colors]
+            [quo2.theme :as theme]))
+
+(def tab-background-opacity 0.3)
+
+(defn size->padding-left
+  [size]
+  (case size
+    32 12
+    28 12
+    24 8
+    20 8
+    nil))
+
+(defn size->border-radius
+  [size]
+  (case size
+    32 10
+    28 8
+    24 8
+    20 6
+    nil))
+
+(def notification-dot
+  {:position :absolute
+   :top      -2
+   :right    -2})
+
+(def container
+  {:flex-direction :row})
+
+(defn tab
+  [{:keys [size disabled background-color show-notification-dot?]}]
+  (let [border-radius (size->border-radius size)
+        padding       (size->padding-left size)]
+    (merge {:height                    size
+            :align-items               :center
+            :flex-direction            :row
+            :border-top-left-radius    border-radius
+            :border-bottom-left-radius border-radius
+            :background-color          background-color
+            :padding-left              padding}
+           ;; The minimum padding right of 1 is a mandatory workaround. Without
+           ;; it, the SVG rendered besides the tab will have a 1px margin. This
+           ;; issue still exists in the latest react-native-svg versions.
+           (if show-notification-dot?
+             {:padding-right 1}
+             {:border-radius border-radius
+              :padding-right padding})
+           (when disabled
+             {:opacity tab-background-opacity}))))
+
+(def themes
+  {:light {:default  {:background-color colors/neutral-20
+                      :icon-color       colors/neutral-50
+                      :label            {:style {:color colors/neutral-100}}}
+           :active   {:background-color colors/neutral-50
+                      :icon-color       colors/white
+                      :label            {:style {:color colors/white}}}
+           :disabled {:background-color colors/neutral-20
+                      :icon-color       colors/neutral-50
+                      :label            {:style {:color colors/neutral-100}}}}
+   :dark  {:default  {:background-color colors/neutral-80
+                      :icon-color       colors/neutral-40
+                      :label            {:style {:color colors/white}}}
+           :active   {:background-color colors/neutral-60
+                      :icon-color       colors/white
+                      :label            {:style {:color colors/white}}}
+           :disabled {:background-color colors/neutral-80
+                      :icon-color       colors/neutral-40
+                      :label            {:style {:color colors/white}}}}})
+
+(def themes-for-blur-background
+  {:light {:default  {:background-color colors/neutral-80-opa-5
+                      :icon-color       colors/neutral-80-opa-40
+                      :label            {:style {:color colors/neutral-100}}}
+           :active   {:background-color colors/neutral-80-opa-60
+                      :icon-color       colors/white
+                      :label            {:style {:color colors/white}}}
+           :disabled {:background-color colors/neutral-80-opa-5
+                      :icon-color       colors/neutral-80-opa-40
+                      :label            {:style {:color colors/neutral-100}}}}
+   :dark  {:default  {:background-color colors/white-opa-5
+                      :icon-color       colors/white
+                      :label            {:style {:color colors/white}}}
+           :active   {:background-color colors/white-opa-20
+                      :icon-color       colors/white
+                      :label            {:style {:color colors/white}}}
+           :disabled {:background-color colors/white-opa-5
+                      :icon-color       colors/neutral-40
+                      :label            {:style {:color colors/white}}}}})
+
+(defn by-theme
+  [{:keys [override-theme disabled active blur?]}]
+  (let [state (cond disabled :disabled
+                    active   :active
+                    :else    :default)
+        theme (or override-theme (theme/get-theme))]
+    (get-in (if blur? themes-for-blur-background themes)
+            [theme state])))

src/quo2/components/tabs/tab/view.cljs
@@ -1,0 +1,94 @@
+(ns quo2.components.tabs.tab.view
+  (:require [quo2.components.icon :as icons]
+            [quo2.components.markdown.text :as text]
+            [quo2.components.tabs.tab.style :as style]
+            [quo2.components.notifications.notification-dot :as notification-dot]
+            [react-native.core :as rn]
+            [react-native.svg :as svg]))
+
+(defn- right-side-with-cutout
+  "SVG exported from Figma."
+  [{:keys [height width background-color disabled]}]
+  ;; Do not add a view-box property, it'll cause an artifact where the SVG is
+  ;; rendered slightly smaller than the proper width and height.
+  [svg/svg
+   {:width        width
+    :height       height
+    :fill         background-color
+    :fill-opacity (when disabled style/tab-background-opacity)}
+   [svg/path
+    {:d
+     "M 11.468 6.781 C 11.004 6.923 10.511 7 10 7 C 7.239 7 5 4.761 5 2 C 5
+     1.489 5.077 0.996 5.219 0.532 C 4.687 0.351 4.134 0.213 3.564 0.123 C 2.787
+     0 1.858 0 0 0 L 0 32 C 1.858 32 2.787 32 3.564 31.877 C 7.843 31.199 11.199
+     27.843 11.877 23.564 C 12 22.787 12 21.858 12 20 L 12 12 C 12 10.142 12
+     9.213 11.877 8.436 C 11.787 7.866 11.649 7.313 11.468 6.781 Z"
+     :clip-path "url(#clip0_5514_84289)"}]
+   [svg/defs
+    [svg/clippath {:id "clip0_5514_84289"}
+     [svg/rect {:width width :height height :fill :none}]]]])
+
+(defn- content
+  [{:keys [size label]} children]
+  [rn/view
+   (cond
+     (string? children)
+     [text/text
+      (merge {:size            (case size
+                                 24 :paragraph-2
+                                 20 :label
+                                 nil)
+              :weight          :medium
+              :number-of-lines 1}
+             label)
+      children]
+
+     (vector? children)
+     children)])
+
+(defn view
+  [{:keys [accessibility-label
+           active
+           before
+           blur?
+           disabled
+           id
+           on-press
+           override-theme
+           size
+           notification-dot?]
+    :or   {size 32}}
+   children]
+  (let [show-notification-dot? (and notification-dot? (= size 32))
+        {:keys [icon-color
+                background-color
+                label]}
+        (style/by-theme {:override-theme override-theme
+                         :blur?          blur?
+                         :disabled       disabled
+                         :active         active})]
+    [rn/touchable-without-feedback
+     (merge {:disabled            disabled
+             :accessibility-label accessibility-label}
+            (when on-press
+              {:on-press (fn []
+                           (on-press id))}))
+     [rn/view {:style style/container}
+      (when show-notification-dot?
+        [notification-dot/notification-dot
+         {:style style/notification-dot}])
+      [rn/view
+       {:style (style/tab {:size                   size
+                           :disabled               disabled
+                           :background-color       background-color
+                           :show-notification-dot? show-notification-dot?})}
+       (when before
+         [rn/view
+          [icons/icon before {:color icon-color}]])
+       [content {:size size :label label} children]]
+      (when show-notification-dot?
+        [right-side-with-cutout
+         {:width            (style/size->padding-left size)
+          :height           size
+          :disabled         disabled
+          :background-color background-color}])]]))

src/status_im2/contexts/activity_center/view.cljs
@@ -50,7 +50,8 @@
 
 (defn tabs
   []
-  (let [filter-type (rf/sub [:activity-center/filter-type])]
+  (let [filter-type       (rf/sub [:activity-center/filter-type])
+        types-with-unread (rf/sub [:activity-center/notification-types-with-unread])]
     [quo/tabs
      {:size                32
       :scrollable?         true
@@ -65,22 +66,39 @@
       :default-active      filter-type
       :data                [{:id    types/no-type
                              :label (i18n/label :t/all)}
-                            {:id    types/admin
-                             :label (i18n/label :t/admin)}
-                            {:id    types/mention
-                             :label (i18n/label :t/mentions)}
-                            {:id    types/reply
-                             :label (i18n/label :t/replies)}
-                            {:id    types/contact-request
-                             :label (i18n/label :t/contact-requests)}
-                            {:id    types/contact-verification
-                             :label (i18n/label :t/identity-verification)}
-                            {:id    types/tx
-                             :label (i18n/label :t/transactions)}
-                            {:id    types/membership
-                             :label (i18n/label :t/membership)}
-                            {:id    types/system
-                             :label (i18n/label :t/system)}]}]))
+                            {:id                  types/admin
+                             :label               (i18n/label :t/admin)
+                             :accessibility-label :tab-admin
+                             :notification-dot?   (contains? types-with-unread types/admin)}
+                            {:id                  types/mention
+                             :label               (i18n/label :t/mentions)
+                             :accessibility-label :tab-mention
+                             :notification-dot?   (contains? types-with-unread types/mention)}
+                            {:id                  types/reply
+                             :label               (i18n/label :t/replies)
+                             :accessibility-label :tab-reply
+                             :notification-dot?   (contains? types-with-unread types/reply)}
+                            {:id                  types/contact-request
+                             :label               (i18n/label :t/contact-requests)
+                             :accessibility-label :tab-contact-request
+                             :notification-dot?   (contains? types-with-unread types/contact-request)}
+                            {:id                  types/contact-verification
+                             :label               (i18n/label :t/identity-verification)
+                             :accessibility-label :tab-contact-verification
+                             :notification-dot?   (contains? types-with-unread
+                                                             types/contact-verification)}
+                            {:id                  types/tx
+                             :label               (i18n/label :t/transactions)
+                             :accessibility-label :tab-tx
+                             :notification-dot?   (contains? types-with-unread types/tx)}
+                            {:id                  types/membership
+                             :label               (i18n/label :t/membership)
+                             :accessibility-label :tab-membership
+                             :notification-dot?   (contains? types-with-unread types/membership)}
+                            {:id                  types/system
+                             :label               (i18n/label :t/system)
+                             :accessibility-label :tab-system
+                             :notification-dot?   (contains? types-with-unread types/system)}]}]))
 
 (defn header
   []

src/mocks/js_dependencies.cljs
@@ -137,10 +137,13 @@ globalThis.__STATUS_MOBILE_JS_IDENTITY_PROXY__ = new Proxy({}, {get() { return (
 (def react-native-share #js {:default {}})
 (def react-native-svg
   #js
-   {:SvgUri  #js {:render identity}
-    :SvgXml  #js {:render identity}
-    :default #js {:render identity}
-    :Path    #js {:render identity}})
+   {:ClipPath #js {:render identity}
+    :Defs     #js {:render identity}
+    :Path     #js {:render identity}
+    :Rect     #js {:render identity}
+    :SvgUri   #js {:render identity}
+    :SvgXml   #js {:render identity}
+    :default  #js {:render identity}})
 (def react-native-webview #js {:default {}})
 (def react-native-audio-toolkit #js {:MediaStates {}})
 (def net-info #js {})

src/quo2/components/notifications/notification_dot.cljs
@@ -2,13 +2,17 @@
   (:require [quo2.foundations.colors :as colors]
             [react-native.core :as rn]))
 
+(def ^:const size 8)
+
 (defn notification-dot
-  [style]
+  [{:keys [style]}]
   [rn/view
-   {:style (merge {:background-color (colors/theme-colors colors/primary-50 colors/primary-60)
-                   :width            8
-                   :height           8
-                   :border-radius    4
-                   :position         :absolute
-                   :z-index          1}
-                  style)}])
+   {:accessibility-label :notification-dot
+    :style               (merge
+                          {:background-color (colors/theme-colors colors/primary-50 colors/primary-60)
+                           :width            size
+                           :height           size
+                           :border-radius    (/ size 2)
+                           :position         :absolute
+                           :z-index          1}
+                          style)}])

src/quo2/components/tabs/tab.cljs
@@ -1,109 +1,0 @@
-(ns quo2.components.tabs.tab
-  (:require [quo2.components.icon :as icons]
-            [quo2.components.markdown.text :as text]
-            [quo2.foundations.colors :as colors]
-            [quo2.theme :as theme]
-            [react-native.core :as rn]))
-
-(def themes
-  {:light {:default  {:background-color colors/neutral-20
-                      :icon-color       colors/neutral-50
-                      :label            {:style {:color colors/neutral-100}}}
-           :active   {:background-color colors/neutral-50
-                      :icon-color       colors/white
-                      :label            {:style {:color colors/white}}}
-           :disabled {:background-color colors/neutral-20
-                      :icon-color       colors/neutral-50
-                      :label            {:style {:color colors/neutral-100}}}}
-   :dark  {:default  {:background-color colors/neutral-80
-                      :icon-color       colors/neutral-40
-                      :label            {:style {:color colors/white}}}
-           :active   {:background-color colors/neutral-60
-                      :icon-color       colors/white
-                      :label            {:style {:color colors/white}}}
-           :disabled {:background-color colors/neutral-80
-                      :icon-color       colors/neutral-40
-                      :label            {:style {:color colors/white}}}}})
-
-(def themes-for-blur-background
-  {:light {:default  {:background-color colors/neutral-80-opa-5
-                      :icon-color       colors/neutral-80-opa-40
-                      :label            {:style {:color colors/neutral-100}}}
-           :active   {:background-color colors/neutral-80-opa-60
-                      :icon-color       colors/white
-                      :label            {:style {:color colors/white}}}
-           :disabled {:background-color colors/neutral-80-opa-5
-                      :icon-color       colors/neutral-80-opa-40
-                      :label            {:style {:color colors/neutral-100}}}}
-   :dark  {:default  {:background-color colors/white-opa-5
-                      :icon-color       colors/white
-                      :label            {:style {:color colors/white}}}
-           :active   {:background-color colors/white-opa-20
-                      :icon-color       colors/white
-                      :label            {:style {:color colors/white}}}
-           :disabled {:background-color colors/white-opa-5
-                      :icon-color       colors/neutral-40
-                      :label            {:style {:color colors/white}}}}})
-
-(defn style-container
-  [size disabled background-color]
-  (merge {:height             size
-          :align-items        :center
-          :justify-content    :center
-          :flex-direction     :row
-          :border-radius      (case size
-                                32 10
-                                28 8
-                                24 8
-                                20 6)
-          :background-color   background-color
-          :padding-horizontal (case size
-                                32 12
-                                28 12
-                                24 8
-                                20 8)}
-         (when disabled
-           {:opacity 0.3})))
-
-(defn tab
-  "[tab opts \"label\"]
-   opts
-   {:type :primary/:secondary/:grey/:outline/:ghost/:danger
-    :size 40/32/24
-    :icon true/false
-    :before :icon-keyword
-    :after :icon-keyword}"
-  [_ _]
-  (fn [{:keys [id on-press disabled size before active accessibility-label blur? override-theme]
-        :or   {size 32}}
-       children]
-    (let [state                                       (cond disabled :disabled
-                                                            active   :active
-                                                            :else    :default)
-          {:keys [icon-color background-color label]}
-          (get-in (if blur? themes-for-blur-background themes)
-                  [(or override-theme (theme/get-theme)) state])]
-      [rn/touchable-without-feedback
-       (merge {:disabled            disabled
-               :accessibility-label accessibility-label}
-              (when on-press
-                {:on-press (fn []
-                             (on-press id))}))
-       [rn/view {:style (style-container size disabled background-color)}
-        (when before
-          [rn/view
-           [icons/icon before {:color icon-color}]])
-        [rn/view
-         (cond
-           (string? children)
-           [text/text
-            (merge {:size            (case size
-                                       24 :paragraph-2
-                                       20 :label
-                                       nil)
-                    :weight          :medium
-                    :number-of-lines 1}
-                   label)
-            children]
-           (vector? children)
-           children)]]])))

commit 003072da453e4e0f8980e6b09314142db13c0f5f

Fix segmented tab component (#15001)
Fixes #14999 

Fix UI regression in the segmented tab component, introduced by PR https://github.com/status-im/status-mobile/pull/14883.

No area was impacted because segmented tabs are not used yet.

src/quo2/components/tabs/segmented_tab.cljs
@@ -29,11 +29,12 @@
             {:margin-left (if (= 0 indx) 0 2)
              :flex        1}
             [tab/view
-             {:id        id
-              :segmented true
-              :size      size
-              :active    (= id active-id)
-              :on-press  #(do (reset! active-tab-id %)
-                              (when on-change
-                                (on-change %)))}
+             {:id         id
+              :segmented? true
+              :size       size
+              :active     (= id active-id)
+              :on-press   (fn [tab-id]
+                            (reset! active-tab-id tab-id)
+                            (when on-change
+                              (on-change tab-id)))}
              label]])]))))

src/quo2/components/tabs/tab/style.cljs
@@ -31,11 +31,16 @@
   {:flex-direction :row})
 
 (defn tab
-  [{:keys [size disabled background-color show-notification-dot?]}]
+  [{:keys [background-color
+           disabled
+           segmented?
+           show-notification-dot?
+           size]}]
   (let [border-radius (size->border-radius size)
         padding       (size->padding-left size)]
     (merge {:height                    size
             :align-items               :center
+            :justify-content           :center
             :flex-direction            :row
             :border-top-left-radius    border-radius
             :border-bottom-left-radius border-radius
@@ -48,6 +53,8 @@
              {:padding-right 1}
              {:border-radius border-radius
               :padding-right padding})
+           (when segmented?
+             {:flex 1})
            (when disabled
              {:opacity tab-background-opacity}))))
 

src/quo2/components/tabs/tab/view.cljs
@@ -55,6 +55,7 @@
            id
            on-press
            override-theme
+           segmented?
            size
            notification-dot?]
     :or   {size 32}}
@@ -80,6 +81,7 @@
       [rn/view
        {:style (style/tab {:size                   size
                            :disabled               disabled
+                           :segmented?             segmented?
                            :background-color       background-color
                            :show-notification-dot? show-notification-dot?})}
        (when before